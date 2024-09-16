1595a44510fbb6a5d9826515203d494ab60160c9
DrD <78091157+otavioomattos@users.noreply.github.com>
2024-09-16 18:35:15 -0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,9 +2,7 @@ RSGCore = exports['rsg-core']:GetCoreObject()
 
 local VORPcore = {}
 
-
 VORPcore.RpcCall = function(name, cb, ...)
-
     if cb and type(cb) == 'function' then
         RSGCore.Functions.TriggerCallback(name, function(result)
             cb(result)
@@ -19,6 +17,28 @@ VORPcore.RpcCall = function(name, cb, ...)
         return Citizen.Await(p)
     end
 end
+
+VORPcore.Callback = {
+    Register = function(name, callback)
+        RSGCore.Functions.CreateCallback(name, callback)
+    end,
+
+    TriggerAsync = function(name, callback, ...)
+        RSGCore.Functions.TriggerCallback(name, function(result)
+            callback(result)
+        end, ...)
+    end,
+
+    TriggerAwait = function(name, ...)
+        local p = promise.new()
+
+        RSGCore.Functions.TriggerCallback(name, function(result)
+            p:resolve(result)
+        end, ...)
+
+        return Citizen.Await(p)
+    end
+}
 
 RegisterNetEvent("vorpcharacter:savenew", function(skin)
     TriggerServerEvent("vorpcharacter:SaveClothes", skin)
