8f8288e78c858d2b2326d08f16ebfe254bbc0e68
DrD <78091157+otavioomattos@users.noreply.github.com>
2024-09-17 15:10:27 -0300

    Update version fxmanifest.lua and added ox_lib/init

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,17 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 description 'RSG VorpCore Wrapper'
 author 'DrD'
-version '0.0.1'
+version '0.0.2'
 
 server_script {
+    '@ox_lib/init.lua',
     '@oxmysql/lib/MySQL.lua',
     'server.lua'
 }
 
-client_script 'client.lua'
+client_script {
+    '@ox_lib/init.lua',
+    'client.lua',
+}
 
 lua54 'yes'
