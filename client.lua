f03300d137aaff323f47866d4884341f6a317f1e
DrD <78091157+otavioomattos@users.noreply.github.com>
2024-09-17 15:16:00 -0300

    Update client.lua added notifys bridge

--- client.lua
+++ client.lua
@@ -2,6 +2,70 @@ RSGCore = exports['rsg-core']:GetCoreObject()
 
 local VORPcore = {}
 
+VORPcore.Warning = function(text)
+    print("^3WARNING: ^7" .. tostring(text) .. "^7")
+end,
+
+VORPcore.Error = function(text)
+    print("^1ERROR: ^7" .. tostring(text) .. "^7")
+end,
+
+VORPcore.Success = function(text)
+    print("^2SUCCESS: ^7" .. tostring(text) .. "^7")
+end,
+
+VORPcore.NotifyTip = function(text, duration)
+    return TriggerEvent('rNotify:Tip', text, duration)
+end,
+
+VORPcore.NotifyLeft = function(title, subtitle, dict, icon, duration, colors)
+    TriggerEvent('rNotify:NotifyLeft', title, subtitle, dict, icon, duration)
+end,
+
+VORPcore.NotifyRightTip = function(text, duration)
+    return lib.notify({title = text, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyObjective = function(text, duration)
+    return lib.notify({title = text, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyTop = function(text, location, duration)
+    return TriggerEvent('rNotify:NotifyTop', text, location, duration)
+end,
+
+VORPcore.NotifySimpleTop = function(text, subtitle, duration)
+    return lib.notify({ title = text, description = subtitle, type = 'info', duration = duration})
+end,
+
+VORPcore.NotifyAvanced = function(text, dict, icon, text_color, duration, quality, showquality)
+    return lib.notify({ title = text, type = 'success', icon = icon, iconAnimation = 'shake', duration = duration})
+end,
+
+VORPcore.NotifyCenter = function(text, duration, color)
+    return lib.notify({title = text, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyBottomRight = function(text, duration)
+    return lib.notify({title = text, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyFail = function(text, subtitle, duration)
+    return lib.notify({title = text, description = subtitle, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyDead = function(title, audioRef, audioName, duration)
+    return lib.notify({title = text, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyUpdate = function(title, subtitle, duration)
+    return lib.notify({title = text, description = subtitle, type = "info", duration = duration})
+end,
+
+VORPcore.NotifyWarning = function(title, msg, audioRef, audioName, duration)
+    return lib.notify({title = title, description = msg, type = "info", duration = duration})
+end,
+
 VORPcore.RpcCall = function(name, cb, ...)
     if cb and type(cb) == 'function' then
         RSGCore.Functions.TriggerCallback(name, function(result)
